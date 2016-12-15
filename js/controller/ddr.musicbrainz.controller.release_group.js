e36ef0fb01d7c1fb33815f98204c381ac667652d
Philip Washington Sorst <philip@sorst.net>
2016-12-16 00:53:10 +0100

    Release Group browsing

--- js/controller/ddr.musicbrainz.controller.release_group.js
+++ js/controller/ddr.musicbrainz.controller.release_group.js
@@ -3,8 +3,13 @@
 
     angular.module('ddr.musicbrainz.controllers').controller('ddr.musicbrainz.controller.release_group', Controller);
 
-    Controller.$inject = ['$scope', 'Restangular', '$q', '$timeout', '$location'];
+    Controller.$inject = ['$scope', 'Restangular', '$q', '$timeout', '$location', '$routeParams'];
 
-    function Controller($scope, restangular, $q, $timeout, $location) {
+    function Controller($scope, restangular, $q, $timeout, $location, $routeParams) {
+        restangular.one('release-group', $routeParams.id).get({'inc': 'releases+artist-credits'})
+            .then(function (response) {
+                $scope.releases = response.releases;
+                $scope.setTitle(response.title);
+            })
     }
 })();
